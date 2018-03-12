3c457d7f69c0387b6d3bf3d80337d0cfadee09b3
detterwr <detterwr@rose-hulman.edu>
2018-03-12 14:58:53 -0400

    4

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Will Detterman.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -109,6 +109,19 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+
+    x = point.x
+    y = point.y
+
+    for _ in range(n):
+        line = rg.Line(rg.Point(x,y),rg.Point(x+length,y))
+        line.attach_to(window)
+        y = y + 30
+
+    window.render()
+
+
+
 
 
 def run_test_draw_lines():
@@ -175,6 +188,13 @@ def draw_lines(n, point, window):
     ####################################################################
     # ------------------------------------------------------------------
 
+    acc = -100
+
+    for _ in range(n):
+        line = rg.Line(point, rg.Point(point.x +100, point.y+acc))
+        line.attach_to(window)
+        acc = acc + 100/n
+    window.render()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
